bd981eb859b93ba64af1f27720d14056a49f8e42
adam lounds <adamlounds@gmail.com>
2015-01-09 11:25:43 +0000

    Initial commit

---
+++ slide_config.js
@@ -0,0 +1,34 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Pirum Hackday<br>Spring 2015',
+    subtitle: 'A space to play',
+    eventInfo: {
+      title: 'Pirum hackday',
+      date: '2015-03-27'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Adam Lounds',
+    company: 'Software Engineer<br>Pirum Systems Ltd',
+    gplus: 'http://plus.google.com/+AdamLounds',
+    twitter: '@adamlounds',
+    www: 'http://www.pirum.com',
+    github: 'http://github.com/adamlounds'
+  }
+ ]
+};
+
